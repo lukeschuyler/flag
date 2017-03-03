9e34f88ff7e6b7b556f65071afcf0914e0f270ff
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-03 09:17:11 -0600

    refactored with loops and ternary

--- flag.js
+++ flag.js
@@ -1,20 +1,23 @@
-#!/usr/bin/env/ node
+#!/usr/bin/env/node
 
 const chalk = require('chalk')
 
 const { bgWhite } = chalk.styles
 const { bgRed } = chalk.styles
 
-console.log(chalk.white.bgBlue('* * * * * * *') + bgRed.open + `                                     ` + bgRed.close)
-console.log(chalk.white.bgBlue(' * * * * * * ') + bgWhite.open + `                                     ` + bgWhite.close)
-console.log(chalk.white.bgBlue('* * * * * * *') + bgRed.open + `                                     ` + bgRed.close)
-console.log(chalk.white.bgBlue(' * * * * * * ') + bgWhite.open + `                                     ` + bgWhite.close)
-console.log(chalk.white.bgBlue('* * * * * * *') + bgRed.open + `                                     ` + bgRed.close)
-console.log(chalk.white.bgBlue(' * * * * * * ') + bgWhite.open + `                                     ` + bgWhite.close)
-console.log(chalk.white.bgBlue('* * * * * * *') + bgRed.open + `                                     ` + bgRed.close)
-console.log(bgWhite.open + `                                                  ` + bgWhite.close)
-console.log(bgRed.open + `                                                  ` + bgRed.close)
-console.log(bgWhite.open + `                                                  ` + bgWhite.close)
-console.log(bgRed.open + `                                                  ` + bgRed.close)
-console.log(bgWhite.open + `                                                  ` + bgWhite.close)
-console.log(bgRed.open + `                                                  ` + bgRed.close)
+const shortWhite = () => { console.log(chalk.white.bgBlue(' * * * * * * * ') + bgWhite.open + `                                     ` + bgWhite.close) }
+const shortRed = () => { console.log(chalk.white.bgBlue('  * * * * * *  ') + bgRed.open + `                                     ` + bgRed.close) }
+const longWhite = () => { console.log(bgWhite.open + `                                                    ` + bgWhite.close) }
+const longRed = () => { console.log(bgRed.open + `                                                    ` + bgRed.close) }
+
+function writeFlag() {
+	for (var i = 0; i < 13; i++) {
+		if (i < 7) {
+			(i % 2 === 0) ? shortWhite() : shortRed()
+		} else {
+			(i % 2 === 0) ? longWhite() : longRed()
+		}
+	}
+}
+
+writeFlag()
